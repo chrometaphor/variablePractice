843c4d744bde740bb912a6f5e89afea3bbd71b21
Chris Cole <christopher.a.cole@gmail.com>
2022-11-08 19:42:45 -0800

    add console logs

--- variables.js
+++ variables.js
@@ -1,5 +1,11 @@
-var email ="christopher.a.cole@gmail.com"
+var email ="christopher.a.cole@gmail.com" 
+console.log(email)
 
 var startingAge = 41
+console.log(startingAge)
 
 var depositPaid = true
+console.log(depositPaid)
+
+email = "tony@gmail.com"
+console.log(email)
